336106eeeb9db8f0a0abd4a7a18fca3a9b55b41b
Pranith chowdary Karumanchi <pranithtpm@gmail.com>
2025-05-20 12:14:44 +0530

    Add unit tests for environments, transformations, and eval datasets

---
+++ unit_tests/eval_dataset_test.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2024 Pranith Chowdary K.
+
+
+import os
+
+os.environ['BOARD_SIZE'] = str(9)
+from alpha_zero.core.eval_dataset import build_eval_dataset
+from alpha_zero.utils.util import create_logger
+
+if __name__ == '__main__':
+    logger = create_logger('DEBUG')
+    eval_dataset = build_eval_dataset('./pro_games/go/9x9', num_stack=8, logger=logger)
+    # eval_dataset = build_eval_dataset('./9x9_matches', num_stack=8, logger=logger)
